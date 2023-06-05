f7c0368522d0065f907c758c167d41d83e0f8f61
core <charfull.mail@gmail.com>
2023-06-05 08:46:28 +0300

    start 35 cw_docker #5 work_local secrets setings 84.201.132.41

--- todolist/settings.py
+++ todolist/settings.py
@@ -84,28 +84,28 @@ WSGI_APPLICATION = 'todolist.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': os.environ.get('SQL_DB_NAME'),
-#         'USER': os.environ.get('SQL_DB_USER'),
-#         'PASSWORD': os.environ.get('SQL_DB_PASSWORD'),
-#         'HOST': os.environ.get('SQL_DB_HOST'),
-#         'PORT': os.environ.get('SQL_DB_PORT'),
-#     }
-# }
-
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'postgres',
-        'USER': 'postgres',
-        'PASSWORD': 'postgres',
-        'HOST': 'pgdb',
-        'PORT': '5432',
+        'NAME': os.environ.get('SQL_DB_NAME'),
+        'USER': os.environ.get('SQL_DB_USER'),
+        'PASSWORD': os.environ.get('SQL_DB_PASSWORD'),
+        'HOST': os.environ.get('SQL_DB_HOST'),
+        'PORT': os.environ.get('SQL_DB_PORT'),
     }
 }
+
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'postgres',
+#         'USER': 'postgres',
+#         'PASSWORD': 'postgres',
+#         'HOST': 'pgdb',
+#         'PORT': '5432',
+#     }
+# }
 
 # DATABASES = {
 #     'default': {
