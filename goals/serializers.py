aeeeefcfe12958d11bb60e0ef73d5715ce5b0284
core <charfull.mail@gmail.com>
2023-06-09 20:16:13 +0300

    HW_36 models category goal

--- goals/serializers.py
+++ goals/serializers.py
@@ -1,7 +1,8 @@
 from rest_framework import serializers
+from rest_framework.exceptions import ValidationError, PermissionDenied
 
 from core.serializers import UserSerializer
-from goals.models import GoalCategory
+from goals.models import GoalCategory, Goal
 
 
 class GoalCategorySerializer(serializers.ModelSerializer):
@@ -17,3 +18,24 @@ class GoalCategoryWithUserSerializer(GoalCategorySerializer):
     user = UserSerializer(read_only=True)
 
 
+class GoalSerializer(serializers.ModelSerializer):
+    user = serializers.HiddenField(default=serializers.CurrentUserDefault())
+
+    class Meta:
+        model = Goal
+        fields = '__all__'
+        read_only_fields = ('id', 'created', 'updated', 'user')
+
+
+    def validated_categry(self, value: GoalCategory) -> GoalCategory:
+        if value.is_deleted:
+            raise ValidationError('Category not found')
+
+        if self.context['request'].user.id != value.user_id:
+            raise PermissionDenied
+        return value
+
+    # Проверка дедлайна
+
+class GoalWithUserSerializer(GoalSerializer):
+    user = UserSerializer(read_only=True)
--- goals/urls.py
+++ goals/urls.py
@@ -1,9 +1,16 @@
 from django.urls import path
 
 # from .views import goal_category
-from .views.goal_category import GoalCategoryListView, GoalCategoryCreateView
+from .views.goal_category import GoalCategoryListView, GoalCategoryCreateView, GoalCategoryDetailView
+from goals.views.goals import GoalListView, GoalCreateView, GoaDetailView
+
 
 urlpatterns = [
     path('goal_category/create', GoalCategoryCreateView.as_view(), name='create_category'),
     path('goal_category/list', GoalCategoryListView.as_view(), name='category_list'),
+    path('goal_category/<int:pk>', GoalCategoryDetailView.as_view(), name='category_detail'),
+
+    path('goal/create', GoalCreateView.as_view(), name='create_goal'),
+    path('goal/list', GoalListView.as_view(), name='goal_list'),
+    path('goal/<int:pk>', GoaDetailView.as_view(), name='goal_detail'),
 ]
