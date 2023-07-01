dc05b24c6485195b4000d0c3f9b9aa632867fb7b
core <charfull.mail@gmail.com>
2023-07-01 18:35:43 +0300

    first start bot on SL new boar view

--- goals/views/board.py
+++ goals/views/board.py
@@ -6,8 +6,40 @@ from goals.models import BoardParticipant, Board, Goal
 from goals.permissions import BoardPermission
 from goals.serializers import BoardSerializer, BoardParticipantSerializer, BoardWithParticipantsSerializer
 
+#
+# class BoardCreateViews(generics.CreateAPIView):
+#     permission_classes = [permissions.IsAuthenticated]
+#     serializer_class = BoardSerializer
+#
+#     def perform_create(self, serializer: BoardSerializer) -> None:
+#         with transaction.atomic():
+#             board = serializer.save()
+#             BoardParticipant.objects.create(user=self.request.user, board=board, role=BoardParticipant.Role.owner)
+#
+#
+# class BoardListView(generics.ListAPIView):
+#     permission_classes = [permissions.IsAuthenticated]
+#     serializer_class = BoardSerializer
+#     filter_backends = [filters.OrderingFilter]
+#     ordering = ['title']
+#
+#     def get_queryset(self) -> QuerySet[Board]:
+#         return Board.objects.filter(participants__user=self.request.user).exclude(is_deleted=True)
+#
+#
+# class BoardDetailView(generics.RetrieveUpdateAPIView):
+#     permission_classes = [BoardPermission]
+#     serializer_class = BoardWithParticipantsSerializer
+#     queryset = Board.objects.prefetch_related('participants__user').exclude(is_deleted=True)
+#
+#     def perform_destroy(self, instance: Board) -> None:
+#         with transaction.atomic():
+#             Board.objects.filter(id=instance.id).update(is_deleted=True)
+#             instance.categories.update(is_deleted=True)
+#             Goal.objects.filter(category__board=instance).update(status=Goal.Status.archived)
 
-class BoardCreateViews(generics.CreateAPIView):
+
+class BoardCreateView(generics.CreateAPIView):
     permission_classes = [permissions.IsAuthenticated]
     serializer_class = BoardSerializer
 
@@ -27,7 +59,7 @@ class BoardListView(generics.ListAPIView):
         return Board.objects.filter(participants__user=self.request.user).exclude(is_deleted=True)
 
 
-class BoardDetailView(generics.RetrieveUpdateAPIView):
+class BoardDetailView(generics.RetrieveUpdateDestroyAPIView):
     permission_classes = [BoardPermission]
     serializer_class = BoardWithParticipantsSerializer
     queryset = Board.objects.prefetch_related('participants__user').exclude(is_deleted=True)
@@ -37,7 +69,6 @@ class BoardDetailView(generics.RetrieveUpdateAPIView):
             Board.objects.filter(id=instance.id).update(is_deleted=True)
             instance.categories.update(is_deleted=True)
             Goal.objects.filter(category__board=instance).update(status=Goal.Status.archived)
-
 
 
 
